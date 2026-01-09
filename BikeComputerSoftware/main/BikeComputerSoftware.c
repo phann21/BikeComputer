8ce7675fde82620f9740eeb23f6a69984ca8cd93
Philip Han <prh5154@psu.edu>
2026-01-09 04:27:36 -0500

    added i2c library

--- BikeComputerSoftware/main/BikeComputerSoftware.c
+++ BikeComputerSoftware/main/BikeComputerSoftware.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+#include "driver/i2c_master.h"
+
 void app_main(void)
 {
+    char *ourTaskName = pcTaskGetName(NULL);
+    ESP_LOGI(ourTaskName, "Hello, Starting up!\n");
 
+    while (1){
+        ESP_LOGI(ourTaskName, "Ha got eem!\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
